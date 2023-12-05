53feee20fe21daf1ee2b9a75d9e494e36bef9b9f
Damien Le Moal <dlemoal@kernel.org>
2023-12-05 10:35:14 +0900

    board: radxa: New Rock 5B configuration
    Add a configuration to use the Radxa rock5b either as a PCI endpoint
    or as a RC host. By default, the endpoint image is built. To build the
    RC host image, run "make RCHOST=y"
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

---
+++ board/radxa/rock5b-ep/overlay/root/pci-ep/epf-reset.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+source "$(dirname "$0")/epf-get-dev.sh"
+dev=$(get_dev)
+
+if [ -e "/sys/bus/pci/drivers/nvme/${dev}" ]; then
+	echo "Unbind ${dev}..."
+	echo -n "${dev}" > /sys/bus/pci/drivers/nvme/unbind
+	sleep 0.5
+
+	# Reset device
+	echo "Resetting ${dev}..."
+	echo bus > /sys/bus/pci/devices/${dev}/reset_method
+	echo 1 > /sys/bus/pci/devices/${dev}/reset
+	sleep 0.5
+
+	echo "Remove ${dev} and rescan..."
+	echo 1 > /sys/bus/pci/devices/${dev}/remove
+	sleep 0.5
+fi
+
+echo "PCI rescan..."
+echo 1 > "/sys/bus/pci/rescan"
+
+dev="$(get_dev)"
+echo "Bind ${dev}..."
+echo -n "${dev}" > /sys/bus/pci/drivers/nvme/bind
